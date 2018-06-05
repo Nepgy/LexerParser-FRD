a88bff2eedfbfecc84e2d2a9ab81b3b209041764
iyaki <ivanyaki96@gmail.com>
2018-06-05 15:24:48 -0300

    Agrego Helpers isAlfabetic y isNumeric

--- web-version/lexer.js
+++ web-version/lexer.js
@@ -34,14 +34,38 @@ function Automata (
 var Helpers = Helpers || {
 
     isAlphaNumeric (str) {
-      for (let i = 0; i < str.length; ++i) {
-        let code = str.charCodeAt(i);
-        if (!(code > 47 && code < 58) // numeric (0-9)
-            && !(code > 64 && code < 91) // upper alpha (A-Z)
-            && !(code > 96 && code < 123) // lower alpha (a-z)
-        ) {
-          return false;
+        for (let i = 0; i < str.length; ++i) {
+            let code = str.charCodeAt(i);
+            if (
+                !(code > 47 && code < 58) // numeric (0-9)
+                && !(code > 64 && code < 91) // upper alpha (A-Z)
+                && !(code > 96 && code < 123) // lower alpha (a-z)
+            ) {
+                return false;
+            }
         }
+        return true;
+    },
+
+    isAlphabetic (str) {
+        for (let i = 0; i < str.length; ++i) {
+            let code = str.charCodeAt(i);
+            if (
+                !(code > 64 && code < 91) // upper alpha (A-Z)
+                && !(code > 96 && code < 123) // lower alpha (a-z)
+            ) {
+                return false;
+            }
+      }
+      return true;
+    },
+
+    isNumeric (str) {
+        for (let i = 0; i < str.length; ++i) {
+            let code = str.charCodeAt(i);
+            if (!(code > 47 && code < 58)) {
+                return false;
+            }
       }
       return true;
     },
