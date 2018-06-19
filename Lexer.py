f6a50d8ef49061c7bebb36d28ef222738227e072
iyaki <ivanyaki96@gmail.com>
2018-06-18 22:06:35 -0300

    Agrego validacion para que falle el caso "asd123" utilizando try, except

--- Lexer.py
+++ Lexer.py
@@ -37,6 +37,10 @@ def transicionAlfabetica(estados, estadoActual, input):
     if estadoActual is None:
         return None
 
+    if estadoActual != 0:
+        if input.isdigit():
+            raise Exception('Error')
+
     if (input.isalpha()):
         if estadoActual == 0:
             return estadoActual + 1
@@ -50,6 +54,10 @@ def transicionNumerica(estados, estadoActual, input):
     if estadoActual is None:
         return None
 
+    if estadoActual != 0:
+        if input.isalpha():
+            raise Exception('Error')
+
     if (input.isdigit()):
         if estadoActual == 0:
             return estadoActual + 1
@@ -61,56 +69,59 @@ def transicionNumerica(estados, estadoActual, input):
 
 # Todo arranca aca
 def tokenizer(string):
+    try:
+        automatas = createAutomatas()
+        tokens = []
+        aceptados = []
+        # Se agrega un espacio al final de la cadena para generar que todos los automatas caigan
+        # al estado trampa antes de terminar la ejecucion del lexer
+        string = string + " "
+        acumuladorInputs = ""
 
-    automatas = createAutomatas()
-    tokens = []
-    aceptados = []
-    # Se agrega un espacio al final de la cadena para generar que todos los automatas caigan
-    # al estado trampa antes de terminar la ejecucion del lexer
-    string = string + " "
-    acumuladorInputs = ""
+        for char in string:
 
-    for char in string:
+            nuevosAceptados = []
+            if  acumuladorInputs == "" and char.isspace():
+                continue
 
-        nuevosAceptados = []
-        if  acumuladorInputs == "" and char.isspace():
-            continue
+            for automata in automatas:
+                automata.input(char)
+                if automata.isAceptado():
+                    nuevosAceptados.append(automata)
 
-        for automata in automatas:
-            automata.input(char)
-            if automata.isAceptado():
-                nuevosAceptados.append(automata)
+            todosTrampa = True
+            for automata in automatas:
+                if not automata.trampa():
+                    todosTrampa = False
+                    break
 
-        todosTrampa = True
-        for automata in automatas:
-            if not automata.trampa():
-                todosTrampa = False
-                break
+            if todosTrampa:
+                if acumuladorInputs == "":
+                    # return "Error"
+                    raise Exception('Error')
+                else:
 
-        if todosTrampa:
-            if acumuladorInputs == "":
-                return "Error"
-            else:
-
-                tokens.append((aceptados[0].identificador, acumuladorInputs))
-                for automata in automatas:
-                    automata.reset()
-                aceptados = []
-                acumuladorInputs = ""
-
-                if not char.isspace():
+                    tokens.append((aceptados[0].identificador, acumuladorInputs))
                     for automata in automatas:
-                        automata.input(char)
-                        if automata.isAceptado():
-                            nuevosAceptados.append(automata)
+                        automata.reset()
+                    aceptados = []
+                    acumuladorInputs = ""
+
+                    if not char.isspace():
+                        for automata in automatas:
+                            automata.input(char)
+                            if automata.isAceptado():
+                                nuevosAceptados.append(automata)
 
 
-        # Se utiliza strip() para no agregar espacios ya que NO es util tenerlos en cuenta a la
-        # hora de saber si hemos consumido caracteres luego de haber agregado un token a la lista 
-        acumuladorInputs += char.strip()
-        aceptados = nuevosAceptados
+            # Se utiliza strip() para no agregar espacios ya que NO es util tenerlos en cuenta a la
+            # hora de saber si hemos consumido caracteres luego de haber agregado un token a la lista 
+            acumuladorInputs += char.strip()
+            aceptados = nuevosAceptados
 
-    return tokens
+        return tokens
+    except Exception as error:
+        return str(error)
 
 def createAutomatas():
     automatas = []
